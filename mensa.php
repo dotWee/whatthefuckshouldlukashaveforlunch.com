b978f69099fc6d2abafa6b0d766448230e80a62d
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-10-09 19:56:29 +0200

    Mensa University Regensburg edition

---
+++ mensa.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>What The Fuck Should Lukas Eat For Lunch</title>
+    <link rel="shortcut icon" href="favicon.png">
+    <meta charset="utf-8">
+
+    <!-- Search engine control -->
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="Coding'n personal stuff">
+    <meta name="author" content="Lukas Wolfsteiner">
+    <meta name="publisher" content="Lukas Wolfsteiner">
+    <meta name="copyright" content="Lukas Wolfsteiner">
+    <link rel="canonical" href="https://dotwee.de/">
+    <meta name="keywords"
+          content="Wolfsteiner, Lukas, Lukas Wolfsteiner, Lukas dotwee Wolfsteiner, dotwee, dot, wee, coding, wee coding, dotwee coding">
+
+    <!-- Layout management -->
+    <meta name="theme-color" content="#444444">
+    <meta name="HandheldFriendly" content="True">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <style>
+        html, body {
+            font-family: "Times New Roman", Times, serif;
+            letter-spacing: 0.01rem;
+            text-align: center;
+            overflow: hidden;
+            padding: 0;
+            margin: 0;
+
+            height: 100%;
+            width: 100%;
+        }
+
+        section * {
+            display: block;
+        }
+
+        section {
+            position: relative;
+            top: 50%;
+
+            -webkit-transform: translateY(-50%);
+            -moz-transform: translateY(-50%);
+            -ms-transform: translateY(-50%);
+            -o-transform: translateY(-50%);
+            transform: translateY(-50%);
+            margin: 0 auto;
+
+            max-width: 25rem;
+            min-width: 12rem;
+            width: 70%;
+        }
+
+        section a {
+            line-height: 120%;
+        }
+
+        h1 {
+            font-size: 3rem;
+            margin: 0;
+        }
+
+        h2 {
+            font-size: 2rem;
+            margin: 1em;
+        }
+
+        footer {
+            white-space: nowrap;
+            left: 0;
+            bottom: 0;
+            position: fixed;
+            padding: 0;
+            margin: 0.8rem;
+        }
+
+        footer > * {
+            font-family: "Lucida Console", Monaco, monospace;
+            font-size: 0.80rem;
+            text-align: left;
+            margin: 0.05em;
+            padding: 0;
+        }
+    </style>
+
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-58602415-5', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+</head>
+<body>
+
+<section>
+    <h1>Lukas should have</h1>
+
+    <?php
+
+    /**
+     * Created by IntelliJ IDEA.
+     * User: Lukas
+     * Date: 15.05.2015
+     * Time: 14:07
+     */
+
+    function gimmeSomeFood()
+    {
+        $_csv = file_get_contents('http://www.stwno.de/infomax/daten-extern/csv/UNI-R/40.csv');
+        $_currentDate = date("d.m.Y");
+        $_menuToday = array();
+
+        foreach (explode("\n", $_csv) as $_row) {
+
+            // parse csv row into a array separated by ;
+            $_csvRow = explode(";", $_row);
+
+            // get item from row
+            $_rowItem = $_csvRow[3];
+
+            // get date from row
+            $_rowDate = $_csvRow[0];
+
+            // make sure only today's items get added to the current menu
+            if ($_currentDate == $_rowDate) {
+                $_menuToday[] = $_rowItem;
+            }
+        }
+
+        // return nothing if todays menu is empty
+        if (empty($_menuToday)) {
+            return "nothing";
+        }
+
+        // return item from todays menu
+        $_randomFood = $_menuToday[array_rand($_menuToday)];
+        return $_randomFood;
+    }
+
+    echo "    <h2>" . gimmeSomeFood() . "</h2>";
+
+    ?>
+
+    <h1>for lunch.</h1>
+
+    <a href="?">Lukas doesn't fucking want that</a>
+</section>
+
+<footer>
+    <p>2016, <a href="https://dotwee.de">Lukas Wolfsteiner</a></p>
+
+    <p>(Uni Regensburg Edition)</p>
+    <p>whatthefuckshouldlukashaveforlunch on <a href="https://github.com/dotWee/whatthefuckshouldlukashaveforlunch.com">Github</a>
+    </p>
+</footer>
+
+</body>
+</html>
