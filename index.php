50019d9bb54ab0416e40a121413d820106c18d39
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-05 13:13:55 +0100

    Disable html cache using custom header properties

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+// Disable cache using custom header
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
