1b983f91dce772b16a696dedc308b1eb6341a44e
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-24 20:16:07 +0100

    Move header config to external library config

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-// Disable cache using custom header
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+include "lib/header.php"
 
 ?>
 
---
+++ lib/header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: lukas
+ * Date: 24.02.17
+ * Time: 20:15
+ */
+
+// Disable cache using custom header
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
