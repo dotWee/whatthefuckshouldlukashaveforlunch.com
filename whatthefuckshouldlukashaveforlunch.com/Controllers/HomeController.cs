d1d24cfcba4f10395af7b9eafac3404d010ef090
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-08-10 07:27:04 +0200

    Cleanup imports and fix incorrect value output

--- whatthefuckshouldlukashaveforlunch.com/Controllers/HomeController.cs
+++ whatthefuckshouldlukashaveforlunch.com/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using whatthefuckshouldlukashaveforlunch.com.Models;
 using whatthefuckshouldlukashaveforlunch.com.Models.Helper;
 
@@ -16,8 +11,8 @@ namespace whatthefuckshouldlukashaveforlunch.com.Controllers
             ViewData["Title"] = "What The Fuck Should Lukas Have For Lunch";
             ViewData["Year"] = Year.Value;
 
-			Meal meal = new Meal(DateHelper.CurrentWeeknumber);
-            ViewData["Food"] = meal.GetRandom().Name;
+			var meal = new Meal(DateHelper.CurrentWeeknumber);
+            ViewData["Food"] = meal.RandomItem.Name;
 
             return View();
         }
