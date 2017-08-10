ea3440ae7d2f38331055a56b60859872eeb319be
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-08-10 07:06:10 +0200

    Transformation from encoding ISO-8859-1 to UTF-8

--- whatthefuckshouldlukashaveforlunch.com/Models/Food.cs
+++ whatthefuckshouldlukashaveforlunch.com/Models/Food.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Text;
+
 namespace whatthefuckshouldlukashaveforlunch.com.Models
 {
     public class Food
     {
         public Food(string name, DateTime date)
         {
-            Name = name;
+            Name = ConvertToUtf8(name);
             Date = date;
+        }
+
+        private string ConvertToUtf8(string unformated)
+        {
+			Encoding iso = Encoding.GetEncoding("ISO-8859-1");
+			Encoding utf8 = Encoding.UTF8;
+
+			byte[] utfBytes = utf8.GetBytes(Name);
+			byte[] isoBytes = Encoding.Convert(utf8, iso, utfBytes);
+			return iso.GetString(isoBytes);
         }
 
         public string Name { get; }
