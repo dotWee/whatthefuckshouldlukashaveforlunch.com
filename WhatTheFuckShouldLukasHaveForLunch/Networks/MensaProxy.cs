3d4d20799c9069f7402fb8ac059b210dd4581a85
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2018-08-17 18:42:58 +0200

    Moved MensaProxy into its own namespace

---
+++ WhatTheFuckShouldLukasHaveForLunch/Networks/MensaProxy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using WhatTheFuckShouldLukasHaveForLunch.Models;
+
+namespace WhatTheFuckShouldLukasHaveForLunch.Networks
+{
+    static class MensaProxy
+    {
+        private static readonly DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Item>));
+
+        private static string GetEndpoint(DayOfWeek weekday) => "http://132.199.139.24:9001/mensa/uni/" + Item.WeekdayValuesMap[weekday];
+
+        private static string DownloadJson(DayOfWeek weekday)
+        {
+            string json;
+            using (var webClient = new System.Net.WebClient())
+            {
+                json = webClient.DownloadString(GetEndpoint(weekday));
+            }
+
+            // TODO: validate json
+            Debug.WriteLine($"Downloaded menu for day={weekday}");
+            return json;
+        }
+
+        private static List<Item> ReadItemsFromJson(string json)
+        {
+            List<Item> items;
+            using (var stream = new MemoryStream(System.Text.Encoding.Default.GetBytes(json)))
+            {
+                items = (List<Item>)serializer.ReadObject(stream);
+            }
+
+            return items;
+        }
+
+        public static Menu GetMenu(DayOfWeek weekday)
+        {
+            var json = DownloadJson(weekday);
+
+            var items = ReadItemsFromJson(json);
+
+            Debug.WriteLine($"Menu for day={weekday} has item count={items.Count}");
+            return new Menu(weekday, items);
+        }
+    }
+}
--- WhatTheFuckShouldLukasHaveForLunch/Pages/Index.cshtml.cs
+++ WhatTheFuckShouldLukasHaveForLunch/Pages/Index.cshtml.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.IO;
-using System.Diagnostics;
-using System.Runtime.Serialization.Json;
-using WhatTheFuckShouldLukasHaveForLunch.Models;
+using WhatTheFuckShouldLukasHaveForLunch.Networks;
 
 namespace WhatTheFuckShouldLukasHaveForLunch.Pages
 {
@@ -40,47 +36,6 @@ namespace WhatTheFuckShouldLukasHaveForLunch.Pages
                     ItemName = randomItem.name;
                 }
             }
-        }
-    }
-
-    internal static class MensaProxy
-    {
-        private static readonly DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<Item>));
-
-        private static string GetEndpoint(DayOfWeek weekday) => "http://132.199.139.24:9001/mensa/uni/" + Item.WeekdayValuesMap[weekday];
-
-        private static string DownloadJson(DayOfWeek weekday)
-        {
-            string json;
-            using (var webClient = new System.Net.WebClient())
-            {
-                json = webClient.DownloadString(GetEndpoint(weekday));
-            }
-
-            // TODO: validate json
-            Debug.WriteLine($"Downloaded menu for day={weekday}");
-            return json;
-        }
-
-        private static List<Item> ReadItemsFromJson(string json)
-        {
-            List<Item> items;
-            using (var stream = new MemoryStream(System.Text.Encoding.Default.GetBytes(json)))
-            {
-                items = (List<Item>)serializer.ReadObject(stream);
-            }
-
-            return items;
-        }
-
-        public static Menu GetMenu(DayOfWeek weekday)
-        {
-            var json = DownloadJson(weekday);
-
-            var items = ReadItemsFromJson(json);
-
-            Debug.WriteLine($"Menu for day={weekday} has item count={items.Count}");
-            return new Menu(weekday, items);
         }
     }
 }
